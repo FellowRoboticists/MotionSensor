a5dfb9c8173ef7f4fe300098643616bb978a0782
Dave Sieh <dj0hnve@gmail.com>
2013-12-08 11:21:04 -0700

    Added comments and refactored a bit.

--- src/sketch.ino
+++ src/sketch.ino
@@ -10,7 +10,19 @@
 
 #define SENSOR_DETECT_PIN 7
 #define LED_PIN 13
+
+// How long to wait for no change to identify a stopped status (ms)
 #define STOP_THRESHOLD 1000L
+
+// How long to wait between reading the sensor. You may want to
+// tweak this value or the STOP THRESHOLD depending on how fast
+// the item is being moved. If the thing is moving fast, you may
+// want to shorten this time so you don't miss too many measurements.
+// If the thing is moving slow, you can probably leave this alone
+// or increase it, but you might need to increase the STOP_THRESHOLD.
+// 
+// Think about it.
+#define DELAY_TIME 200
 
 enum MotionState {
   Stopped,
@@ -46,21 +58,29 @@ void loop()
 
   // Now deal with the real world
   if (value != previousValue) {
-    previousValue = value;
-    lastChangeTime = millis();
-    currentState = Moving;
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("Now moving...");
+    handleValueChange(value);
   } else {
-    long duration = sampleTime - lastChangeTime;
-    if (duration > STOP_THRESHOLD) {
-      currentState = Stopped;
-      digitalWrite(LED_PIN, LOW);
-      Serial.print("No change for too long (");
-      Serial.print(duration);
-      Serial.println(") ms. Stopped");
-    }
+    handleValueNoChange(sampleTime);
   }
 
-  delay(300);
+  delay(DELAY_TIME); 
+}
+
+void handleValueChange(int value) {
+  previousValue = value;
+  lastChangeTime = millis();
+  currentState = Moving;
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("Now moving...");
+}
+
+void handleValueNoChange(int sampleTime) {
+  long duration = sampleTime - lastChangeTime;
+  if (duration > STOP_THRESHOLD) {
+    currentState = Stopped;
+    digitalWrite(LED_PIN, LOW);
+    Serial.print("No change for too long (");
+    Serial.print(duration);
+    Serial.println(") ms. Stopped");
+  }
 }
